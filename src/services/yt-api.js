bc45a71eebf1fa7b598730a11275736d70b52f27
Summer Jade <summerjadefrosal@gmail.com>
2022-07-11 01:37:33 -0400

    added bulma

--- src/services/yt-api.js
+++ src/services/yt-api.js
@@ -1,17 +1,22 @@
 let api_key="AIzaSyATp3ljyUuKfR6WLkyPdiMjiHDiCzacc_8"
 let video_http="https://www.googleapis.com/youtube/v3/"
 
+import data from './data.js'
+
 export const getVideos = async () => {
-    return fetch(`${video_http}search?` + new URLSearchParams({
-        key: api_key,
-        part: 'snippet',
-        maxResults: 25,
-        q: 'javascript tutorial'
-    }))
-    .then(res => res.json())
-    .then(data => {
-        return data
-    })
+    return data.data;
+
+    // return fetch(`${video_http}search?` + new URLSearchParams({
+    //     key: api_key,
+    //     part: 'snippet',
+    //     maxResults: 5,
+    //     q: 'javascript tutorial'
+    // }))
+    // .then(res => res.json())
+    // .then(data => {
+    //     return data
+    // })
+
 }
 
 export const getVideo = async () => {
